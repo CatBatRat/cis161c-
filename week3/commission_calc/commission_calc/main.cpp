7778c88471257e042d40df163ebac2c644d5a9b4
tadrith <alexandermeyersaam@gmail.com>
2018-10-12 00:58:43 -0700

    Recommit just to be safe

--- week3/commission_calc/commission_calc/main.cpp
+++ week3/commission_calc/commission_calc/main.cpp
@@ -13,18 +13,30 @@ int main()
 
     // Set base threshold values
     const double base1 = 100000, base2 = 300000;
+    // Set commission base amount per level
+    const double com1 = 5000, com2 = 25000;
     // Set commission percentage rates.
     const double rate0 = 0.05, rate1 = 0.10, rate2 = 0.15;
-    // Set commission base ammount per level
-    const double com1 = 5000, com2 = 25000;
 
     cout << fixed << showpoint << setprecision(2);
+    /* This only needs to be printed once, or I'm just
+     * too lazy to type this for every if statement.
+     * What's the old adage "Programmers are lazy. Why
+     * else would they go to so much trouble just to save
+     * a few minutes"? I'm begging to see what was meant by
+     * that. */
     cout << endl << "Your commission for this sale is $";
 
     // Start at highest point and work down
-    if( input >= base2) { cout << (input - base2) * rate2 + com2; }
-    else if( input >= base1 ) { cout << (input - base1) * rate1 + com1; }
-    else { cout << (input * rate0); }
+    if( input >= base2) {
+        cout << (input - base2) * rate2 + com2;
+    }
+    else if( input >= base1 ) {
+        cout << (input - base1) * rate1 + com1;
+    }
+    else {
+        cout << (input * rate0);
+    }
 
 
     return 0;
