2f5921e3df2433e4e78c365af1a42b3fd39ab220
tadrith <alexandermeyersaam@gmail.com>
2018-10-13 15:32:39 -0700

    Finished both parts of lab 3

--- week3/commission_calc/commission_calc/main.cpp
+++ week3/commission_calc/commission_calc/main.cpp
@@ -3,41 +3,69 @@
 #include <iomanip>
 #include <stdio.h>
 
+/* I think I got a little carried away with this one.
+ * I really liked the idea of the calulations being done
+ * on the fly and there being no limit to the number of
+ * levels this code can calulate.*/
+
 using namespace std;
 
-int main()
-{
-    double input;
-    cout << "Please input the total transaction amount.\n"
-    "Amount: "; cin >> input;
+int main() {
+    // Base amounts at which interest levels change.
+    const double BASES[] = {0, 100000, 300000};
+    // Number of interest levels MUST match number of bases!
+    // Each level applies to the amount OVER the previous BASE;
+    // remember, these are cumlative.
+    const double RATES[] = {0.05, 0.10, 0.15};
 
-    // Set base threshold values
-    const double base1 = 100000, base2 = 300000;
-    // Set commission base amount per level
-    const double com1 = 5000, com2 = 25000;
-    // Set commission percentage rates.
-    const double rate0 = 0.05, rate1 = 0.10, rate2 = 0.15;
+    // define variables
+    double input, output;
 
+    // Variables for:
+    //  RATES array length
+    //  Controlling intry into the second while loop
+    //  Tracking number of loops in relation to RATES array length
+    int rl, go = 1, tr = 0;
+
+    // Get number of elements in the "RATES" array
+    rl = sizeof(RATES)/sizeof(RATES[0]);
+    // I wonder why there is not something like the
+    // String object property for length with arrays.
+
+    // Set display of any following numbers
     cout << fixed << showpoint << setprecision(2);
-    /* This only needs to be printed once, or I'm just
-     * too lazy to type this for every if statement.
-     * What's the old adage "Programmers are lazy. Why
-     * else would they go to so much trouble just to save
-     * a few minutes"? I'm begging to see what was meant by
-     * that. */
-    cout << endl << "Your commission for this sale is $";
 
-    // Start at highest point and work down
-    if( input >= base2) {
-        cout << (input - base2) * rate2 + com2;
+    while(1) {
+    // Hardcoded value for testing
+    //input = 400000;
+    cout << "Please input the total transaction amount\n"
+        "from your sale to calulate your commission.\n"
+    "Amount: "; cin >> input;
+    // Exit program upon entering 0
+    if( input == 0 ) return 0;
+    // Set go = 0 upon negative number entry
+    if( input <= 0 ) {
+        go = 0; cout << "Please enter a positive value\n";
+        break;
     }
-    else if( input >= base1 ) {
-        cout << (input - base1) * rate1 + com1;
-    }
-    else {
-        cout << (input * rate0);
+    else break;
     }
 
+    while(go) {
+        if( input < BASES[tr+1]-BASES[tr] or tr == rl-1 ) {
+            output += input * RATES[tr];
+            cout << endl << "Your commission for this sale is $";
+            cout << output << endl;
+            break;
+        }
+        if( input > BASES[tr] ) {
+            if( tr == rl ) break;
+            output += (BASES[tr+1]-BASES[tr])*RATES[tr];
+            input -= (BASES[tr+1]-BASES[tr]);
+        }
+        else break;
+        tr++;
+    }
 
     return 0;
 }
