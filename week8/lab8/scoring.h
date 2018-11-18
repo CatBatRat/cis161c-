70f0374bfa06884457e5f97fe8e71111d0046662
tadrith <alexandermeyersaam@gmail.com>
2018-11-18 01:14:51 -0800

    Cleaned up header in lab8

--- week8/lab8/scoring.h
+++ week8/lab8/scoring.h
@@ -1,18 +1,18 @@
 #ifndef __SCORING__
 #define __SCORING__
 
-int scoreOnes(int ones, int twos, int threes, int fours, int fives, int sixes);
-int scoreTwos(int ones, int twos, int threes, int fours, int fives, int sixes);
-int scoreThrees(int ones, int twos, int threes, int fours, int fives, int sixes);
-int scoreFours(int ones, int twos, int threes, int fours, int fives, int sixes);
-int scoreFives(int ones, int twos, int threes, int fours, int fives, int sixes);
-int scoreSixes(int ones, int twos, int threes, int fours, int fives, int sixes);
-int scoreThreeOfAKind(int ones, int twos, int threes, int fours, int fives, int sixes);
-int scoreFourOfAKind(int ones, int twos, int threes, int fours, int fives, int sixes);
-int scoreFullHouse(int ones, int twos, int threes, int fours, int fives, int sixes);
-int scoreSmallStraight(int ones, int twos, int threes, int fours, int fives, int sixes);
-int scoreLargeStraight(int ones, int twos, int threes, int fours, int fives, int sixes);
-int scoreYahtzee(int ones, int twos, int threes, int fours, int fives, int sixes);
-int scoreChance(int ones, int twos, int threes, int fours, int fives, int sixes);
+int scoreOnes(int, int, int, int, int, int);
+int scoreTwos(int, int, int, int, int, int);
+int scoreThrees(int, int, int, int, int, int);
+int scoreFours(int, int, int, int, int, int);
+int scoreFives(int, int, int, int, int, int);
+int scoreSixes(int, int, int, int, int, int);
+int scoreThreeOfAKind(int, int, int, int, int, int);
+int scoreFourOfAKind(int, int, int, int, int, int);
+int scoreFullHouse(int, int, int, int, int, int);
+int scoreSmallStraight(int, int, int, int, int, int);
+int scoreLargeStraight(int, int, int, int, int, int);
+int scoreYahtzee(int, int, int, int, int, int);
+int scoreChance(int, int, int, int, int, int);
 
 #endif
